14354af79d00211bad8ab7868bdb118a04ee85cd
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-03-29 23:01:33 +0200

    Update ckFizzBuzzController.py

--- ckFizzBuzzController.py
+++ ckFizzBuzzController.py
@@ -1,33 +1,6 @@
-"""
-Practica 3 ISBC
-
-Applicación Editor de Texto
-
-En este fichero se encuentra el controlador encargado
-de relacionar la vista con el modelo
-
-Author: Marcos Rivera Gavilan
-Website: https://www.uco.es/~i92rigam/
-
-Importante: Para reducir el número de comentarios,
-y simplificar la lectura, solo comentaré las funciones
-nuevas de este ejercicio. El resto que aparezcan sin
-comentar, habrán sido comentadas en ejercicios anteriores.
-"""
 import ckModAppFizzBuzz as mapp
 
 
-def eventSelectFolder(self):
-    mapp.selectFolder(self)
-
-
-def eventClickedOnTreeView(self, index):
-    mapp.on_treeView_clicked(self, index)
-
-
-def eventSaveAsFile(self):
-    mapp.saveAsFile(self)
-
-
-def eventSaveFile(self):
-    mapp.saveFile(self)
+def eventFizzBuzz(self):
+    number = mapp.fizzBuzz(self)
+    self.number.setText(number)
